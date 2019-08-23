8a882c0ce1d4199e16f2a6538b6f4774e6e153cf
[juanantonio.ruiz] <[juanantonio.ruiz@krama.es]>
2019-08-23 18:11:07 +0200

    Iniciar estructura y bbdd

---
+++ src/app/common/constants/database.ts
@@ -0,0 +1,27 @@
+export const audios = [
+    {
+        id: 0,
+        audioName: 'audio001_pk.mp3',
+        title: 'Amigo que paso de ti que borres mi waza',
+        person_id: 0,
+        date: 277871293981,
+        tag_id: 0
+    },
+];
+
+export const persons = [
+    {
+        id: 0,
+        personName: 'richard',
+        img: 'urlImagen.jpg'
+    }
+];
+
+export const tags = [
+    {
+        id: 0,
+        tagName: 'etiqueta 1',
+        position: 0,
+        color: '#FFAA44'
+    }
+]
